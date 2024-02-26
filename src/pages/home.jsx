9d05420ecb1b682152bc4588c10225825d5cca0d
Mohamed Siraj <siraj@oganro.com>
2024-02-26 20:27:58 +0530

    project init

---
+++ src/pages/home.jsx
@@ -0,0 +1,28 @@
+
+
+const Home = () => {
+
+  return (
+    <>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        {/* <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+          </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
