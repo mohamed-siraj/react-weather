af0ebb476906970d500b2f77086b2a2ad3d1f4f7
Mohamed Siraj <siraj@oganro.com>
2024-02-27 10:37:08 +0530

    changes update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-
+import Footer from '../components/footer';
 
 const Home = () => {
 
@@ -6,21 +6,9 @@ const Home = () => {
     <>
       <div className="flex justify-center mt-56">
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img className="rounded-t-lg" src="https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=2048x2048&w=is&k=20&c=65dbok-A-9z5YZ6Hx_B2fbWEavkpEfBbzw7OUVYbvWM=" alt="" />
-          </a>
-          <div className="p-5">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Read more
-              <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </a>
-          </div>
+          <img className="rounded-t-lg" src="https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=2048x2048&w=is&k=20&c=65dbok-A-9z5YZ6Hx_B2fbWEavkpEfBbzw7OUVYbvWM=" alt="" />
+          <Footer/>
+          <Footer/>
         </div>
       </div>
 
